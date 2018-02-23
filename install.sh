893a0e568af395b113e932ab81c3a89b2486b9b5
ViperBlackSkull <simon.nolet@hotmail.com>
2018-02-23 14:48:02 -0500

    fixed dirsearch

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 mkdir ~/Tools/
 #Installation aquatone
+cp dirsearch /usr/bin/dirsearch
 apt install ruby-ffi
 gem install aquatone
 cd ~/Tools/
-wget https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/f58e82c9abfa46a932eb92edbe6b18214141439b/all.txti
+wget https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/f58e82c9abfa46a932eb92edbe6b18214141439b/all.txt
 cd ~/Tools/
 git clone https://github.com/ChrisTruncer/EyeWitness.git
 cd EyeWitness/setup/
 sh setup.sh
 cd ~/Tools/
 git clone https://github.com/maurosoria/dirsearch.git
-echo 'exec /usr/bin/python3 /root/Tools/dirsearch/dirsearch.py "$@"' > 
-/usr/bin/dirsearch
 
